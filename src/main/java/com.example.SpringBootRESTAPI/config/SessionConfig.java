81f0de639919727e8d8b31f703bfc73b129c02cf
toshalSelokar <toshalselokar@gmail.com>
2025-08-05 20:53:36 +0530

    Add files via upload

---
+++ src/main/java/com.example.SpringBootRESTAPI/config/SessionConfig.java
@@ -0,0 +1,22 @@
+package com.example.SpringBootRESTAPI.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+@Configuration
+@EnableJdbcHttpSession
+public class SessionConfig {
+    
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("SESSION");
+        serializer.setCookiePath("/");
+        // Uncomment for domain-specific configuration
+        // serializer.setDomainNamePattern("^.+?\\.(\\w+\\.[a-z]+)$");
+        return serializer;
+    }
+}
